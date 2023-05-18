695e364192ec4c807c03bc7c30ed4c795191f8d7
Zechariah hounwanou <zechariahhounwanou@gmail.com>
2023-05-19 00:03:21 +0100

    Implemented Edit Profile functionality which update the profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 const Profile = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeUserInfo, setChangeUserInfo] = useState(false);
   const [loggedUser, setLoggedUser] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
 
   const { name, email } = loggedUser;
+
+  const handleNameChange = (e) => {
+    setLoggedUser((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleProfileUpdate = async () => {
+    console.log(changeUserInfo);
+    console.log("Just Logging");
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        }); // updating the display name inside of the auth
+
+        const docRef = doc(db, "users", auth.currentUser.uid); // make a copy of all the values available in the collection
+        await updateDoc(docRef, {
+          name,
+        }); // update the name in the user doc of the firestore user collection
+      } // update as long as the display name from auth is different from the name input value
+      toast.success("Profile Updated Successfully");
+    } catch (error) {
+      toast.error("Could Not Update Profile");
+    }
+  };
 
   const handleLogout = () => {
     auth.signOut();
@@ -28,10 +58,13 @@ const Profile = () => {
             <form>
               <input
                 type="text"
-                className=" w-full px-4 py-3 mb-6 text-xl text-gray-800 border border-gray-300 rounded-md bg-white"
+                className={`w-full px-4 py-3 mb-6 text-xl text-gray-800 border border-gray-300 rounded-md bg-white ${
+                  changeUserInfo ? "bg-green-200 focus:bg-green-200" : null
+                }`}
                 id="name"
                 value={name}
-                disabled
+                disabled={!changeUserInfo}
+                onChange={handleNameChange}
               />
               <input
                 type="text"
@@ -41,10 +74,16 @@ const Profile = () => {
                 disabled
               />
               <div className=" flex items-center justify-between text-sm md:text-lg whitespace-nowrap">
-                <p className="  ">
+                <p>
                   Do You Wish To Change Name?
-                  <span className="text-red-600 hover:text-red-700 transition-colors duration-300 cursor-pointer font-semibold ml-2">
-                    Edit
+                  <span
+                    className="text-red-600 hover:text-red-700 transition-colors duration-300 cursor-pointer font-semibold ml-2"
+                    onClick={() => {
+                      changeUserInfo ? handleProfileUpdate() : null;
+                      setChangeUserInfo((prevState) => !prevState);
+                    }}
+                  >
+                    {changeUserInfo ? "Apply Change" : "Edit"}
                   </span>
                 </p>
                 <p
